5a0cee07d24a9b5e21d08eafa08cd32b648a7516
chatapazar <chatapazar@gmail.com>
2017-09-25 11:34:52 +0700

    add printting

--- HelloGit/src/com/test/HelloGit.java
+++ HelloGit/src/com/test/HelloGit.java
@@ -3,9 +3,14 @@ package com.test;
 public class HelloGit {
 
 	public static void main(String[] args) {
+		String[] names = {"a", "b", "c"};
+		
 		//add greeting
-		System.out.println("Hello Git!");
-
+		for (int i=0;i<names.length;i++) {
+			System.out.format("Hello, %s!\n", names[i]);
+	
+		}
+		
 	}
 
 }
