73147b787806f8f7d989de88843c00b411f9b291
chatapazar <chatapazar@gmail.com>
2017-09-25 13:13:11 +0700

    add test

--- HelloGit/src/com/test/HelloGit.java
+++ HelloGit/src/com/test/HelloGit.java
@@ -3,11 +3,12 @@ package com.test;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		String[] names = {"a", "b", "c"};
+		String[] names = {"a", "b", "c", "d"};
 		
 		//add greeting
 		for (int i=0;i<names.length;i++) {
 			System.out.format("Hello, %s!\n", names[i]);
+			System.out.println("Test Test Test");
 	
 		}
 		
