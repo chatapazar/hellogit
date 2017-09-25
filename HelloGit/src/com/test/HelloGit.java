6dd6e9dc8783673c596b4edc2e2fb6af01629ded
chatapazar <chatapazar@gmail.com>
2017-09-25 13:10:34 +0700

    change collection loop

--- HelloGit/src/com/test/HelloGit.java
+++ HelloGit/src/com/test/HelloGit.java
@@ -5,9 +5,9 @@ public class HelloGit {
 	public static void main(String[] args) {
 		String[] names = {"a", "b", "c"};
 		
-		//add greeting
-		for (int i=0;i<names.length;i++) {
-			System.out.format("Hello, %s!\n", names[i]);
+		//add greeting by a
+		for (String name : names) {
+			System.out.format("Hello, %s!\n", name);
 	
 		}
 		
